c2c62e2c251b703ebc6c53b0b6fd2136d55d88a2
Israelshecktar <iamshecktar1996@gmail.com>
2024-07-01 15:05:23 +0000

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Handle connection errors
+    this.client.on('error', (err) => console.error(`Redis client error: ${err}`));
+
+    // Promisify the redis methods
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+// Export an instance of RedisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
