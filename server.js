0520b0f94f908fed25c509e16c662880fc90d9bf
Israelshecktar <iamshecktar1996@gmail.com>
2024-07-03 14:38:39 +0000

    Create a new user

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-// server initialization
+// server.js
 import express from 'express';
 import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
