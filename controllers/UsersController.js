4f08604c4a95a73b976182fc93cd385bbdf3edef
Israelshecktar <iamshecktar1996@gmail.com>
2024-07-03 20:39:43 +0000

    Aded Users to db

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,51 +1,27 @@
+const sha1 = require('sha1');
 const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
 
 class UsersController {
-  static async postNew (req, res) {
+  static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      res.end();
-      return;
-    }
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      res.end();
-      return;
-    }
-    const userExist = await dbClient.userExist(email);
-    if (userExist) {
-      res.status(400).json({ error: 'Already exist' });
-      res.end();
-      return;
-    }
-    const user = await dbClient.createUser(email, password);
-    const id = `${user.insertedId}`;
-    res.status(201).json({ id, email });
-    res.end();
-  }
 
-  static async getMe (req, res) {
-    const token = req.headers['x-token'];
-    if (!token) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
-    const id = await redisClient.get(`auth_${token}`);
-    if (!id) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
     }
-    const user = await dbClient.getUserById(id);
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
+
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
     }
-    res.json({ id: user._id, email: user.email }).end();
+
+    const hashedPassword = sha1(password);
+    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+
+    return res.status(201).json({ id: result.insertedId, email });
   }
 }
 
